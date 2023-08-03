492844084a2fd13ad207861c68ebc5920221bc4d
Profe-Erick-Dalet <131178489+Profe-Erick-Dalet@users.noreply.github.com>
2023-08-03 11:18:57 -0600

    Add model comment
    Signed-off-by: Profe-Erick-Dalet <131178489+Profe-Erick-Dalet@users.noreply.github.com>

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task,Comment
+
 # Register your models here.
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id','title','priority','created_at']
+    #pass
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
